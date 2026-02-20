8d2537bdde9a9ad6cc3d5e373bc8d6987d36aa1a
cavagugo <cavagugo@gmail.com>
2026-02-19 23:07:35 -0600

    Colisiones y fix de movimientos
    Se detectan colisiones con un Raycast en vez de colliders. Si el jugador intenta moverse en diagonal al estar contra un objeto, se lee como si fuera en alguna dirección normal (arriba/abajo, izquierda/derecha)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,8 +17,52 @@ public class Player : MonoBehaviour
     void Update()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * Time.deltaTime * moveSpeed;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.9f;
+        //Testeado desde el editor
+        float playerHeight = 4f;
+        //Si no pega con nada, se puede mover
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove)
+        {
+            //No se puede mover en moveDir (permite movimiento diagonal al colisionar con un objeto)
+
+            //Intentar solo movimiento en X
+            Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f).normalized; //.normalized para que tenga la misma velocidad que si presionara solo A/D
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            if (canMove)
+            {
+                //Solo se puede mover en X
+                moveDir = moveDirX;
+            }
+            else
+            {
+                //No se puede mover solo en X
+
+                //Intentar en movimiento solo Z
+                Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    //Solo se puede mover en Z
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    //No se puede mover en ninguna dirección
+                }
+            }
+        }
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+        
 
         isWalking = moveDir != Vector3.zero;
 
