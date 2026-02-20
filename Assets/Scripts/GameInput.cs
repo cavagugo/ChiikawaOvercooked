f1bd0c07020f73c450c0d4f5a811db1e1eae6432
cavagugo <cavagugo@gmail.com>
2026-02-19 22:22:45 -0600

    Cinemachine y nuevo Input system
    La cámara ahora se maneja con cinemachine, es estática y tiene un ligero movimiento como de cámara en mano. Además, se usa el nuevo input system y permite jugar con mando.

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    private PlayerInputActions playerInputActions;
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+    public Vector2 GetMovementVectorNormalized()
+    {
+        //Hace lo que antes hacían los if de WASD 
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+
+        //Para moverse siempre en la misma dirección (línea recta o diagonales)
+        inputVector = inputVector.normalized;
+
+        return inputVector;
+    }
+}
