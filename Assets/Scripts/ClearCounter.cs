31985cf69effc523c74cfe06b37f8e03af021cfe
cavagugo <cavagugo@gmail.com>
2026-02-23 20:43:41 -0600

    Reconocimiento de objetos de cocina
    Las mesas reconocen que tienen un objeto encima y lo cambian de lugar visualmente y dentro de código. De igual manera, los objetos saben que están asociados a mesas.
    
    El jugador puede mover de lugar un objeto al presionar la tecla T.

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -7,14 +7,56 @@ public class ClearCounter : MonoBehaviour
 
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing;
+
+    private KitchenObject kitchenObject;
+
+    private void Update()
+    {
+        if (testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if (kitchenObject != null)
+            {
+                kitchenObject.SetClearCounter(secondClearCounter);
+            }
+        }
+    }
     public void Interact()
     {
-        Debug.Log("Interact!");
-        //Instanciamos el prefab asociado al SO
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-        //Nos aseguramos que esté bien colocado.
-        kitchenObjectTransform.localPosition = Vector3.zero;
+        //Para que solo se pueda poner un objeto sobre la mesa
+        //Si la mesa está vacía
+        if (kitchenObject == null)
+        {
+            //Instanciamos el prefab asociado al SO
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        }
+        else //Si ya hay algo encima
+        {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }        
+    }
 
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject() { return kitchenObject; }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
     }
 }
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -5,6 +5,33 @@ using UnityEngine;
 public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    private ClearCounter clearCounter;
 
     public KitchenObjectSO GetKitchenObjectSO() { return kitchenObjectSO; }
+
+    //Hacer que cambie de parent/mesa. El parametro que recibe es la nueva mesa
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        //Vamos a la mesa/padre actual y la limpiamos/liberamos
+        if (this.clearCounter != null)
+        {
+            this.clearCounter.ClearKitchenObject();
+        }
+        //Vamos a la nueva mesa y ponemos el objeto que estaba en la mesa anterior
+        this.clearCounter = clearCounter;
+
+        if (clearCounter.HasKitchenObject())
+        {
+            Debug.LogError("La mesa ya tiene un KitchenObject");
+        }
+        clearCounter.SetKitchenObject(this);
+        //Poner el objeto en dicha mesa (visualmente)
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        //Nos aseguramos que esté bien colocado.
+        transform.localPosition = Vector3.zero;
+    }
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
+    }
 }
