d7f05b5d79ae90e2522562140957edecfafc9cbd
cavagugo <cavagugo@gmail.com>
2026-02-27 00:37:20 -0600

    CuttingRecipes
    El jugador puede cortar todas las frutas y recibir la fruta cortada correcta.
    Objetos que no se pueden cortar no pueden ser colocados.
    Objetos que ya se cortaron una vez no pueden volver a ser cortados.

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class CuttingCounter : BaseCounter
 {
-    [SerializeField] private KitchenObjectSO cutKichenObjectSO;
+    [SerializeField] private CuttingRecipeSO[] cuttingRecipeSOArray;
     public override void Interact(Player player)
     {
+        //No hay item sobre la mesa
         if (!HasKitchenObject())
         {
-            //No hay item sobre la mesa
+            //El jugador tiene un item 
             if (player.HasKitchenObject())
             {
-                //El jugador tiene un item y lo coloca
-                player.GetKitchenObject().SetKitchenObjectParent(this);
+                if (HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO()))
+                {
+                    //El item se puede cortar, entonces lo poned
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
+                }  
+                
             }
             else
             {
@@ -37,12 +42,35 @@ public class CuttingCounter : BaseCounter
 
     public override void InteractAlternate(Player player)
     {
-        if (HasKitchenObject())
+        //Hay un objeto Y puede ser cortado (para casos donde el jugador ya haya cortado algo e intente cortarlo de nuevo)
+        if (HasKitchenObject() && HasRecipeWithInput(GetKitchenObject().GetKitchenObjectSO()))
         {
+            //Buscamos el output antes de destruir el objeto
+            KitchenObjectSO outputKitchenObjectSO = GetOutputForInput(GetKitchenObject().GetKitchenObjectSO());
             GetKitchenObject().DestroySelf();
+            
             //Llamamos a la clase porque el método es estático
-            KitchenObject.SpawnKitchenObject(cutKichenObjectSO, this);
+            KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
 
         }
+    }
+
+    private bool HasRecipeWithInput(KitchenObjectSO kitchenObjectSO)
+    {
+        //Regresa true si se puede cortar
+        return !(GetOutputForInput(kitchenObjectSO) == null);
+    }
+
+    //Busca el ingrediente que debemos cortar/preparar en el arreglo
+    private KitchenObjectSO GetOutputForInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        foreach (CuttingRecipeSO cuttingRecipeSO in cuttingRecipeSOArray)
+        {
+            if (cuttingRecipeSO.input == inputKitchenObjectSO)
+            {
+                return cuttingRecipeSO.output;
+            }
+        }
+        return null;
     }
 }
---
+++ Assets/Scripts/CuttingRecipeSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class CuttingRecipeSO : ScriptableObject
+{
+    //Al colocar input y cortar, recibimos output
+    public KitchenObjectSO input;
+    public KitchenObjectSO output;
+
+
+}
