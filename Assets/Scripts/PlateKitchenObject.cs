fe33046a5b1f9a3e099f3444e12369e8dec111f3
cavagugo <cavagugo@gmail.com>
2026-03-01 12:32:24 -0600

    Validity checks
    El jugador siempre debe poner primero la base del pastel (GlazedCake) antes de poder poner cualquier fruta.
    
    El jugador solo podrá poner una fruta una vez tenga la base en el plato.

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -16,35 +16,70 @@ public class PlateKitchenObject : KitchenObject
     }
 
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
+    [SerializeField] private KitchenObjectSO glazedCakeKitchenObjectSO;
 
     private List<KitchenObjectSO> kitchenObjectSOList;
+    private bool hasGlazedCake = false;
+    private bool hasFruit = false;
 
     private void Awake()
     {
        kitchenObjectSOList = new List<KitchenObjectSO>();
     }
+
+    //El primer ingrediente debe ser Glazed Cake, para que visualmente, las frutas no estén volando
     public bool TryAddIngredient(KitchenObjectSO kitchenObjectSO)
     {
-        if (!validKitchenObjectSOList.Contains(kitchenObjectSO))
+        if (hasGlazedCake) //Los siguientes ingredientes que se agreguen siempre serán fruta
         {
-            //No es un ingrediente válido
-            return false;
+            if (!validKitchenObjectSOList.Contains(kitchenObjectSO))
+            {
+                //No es un ingrediente válido
+                return false;
+            }
+
+            if (kitchenObjectSOList.Contains(kitchenObjectSO)) //Ya tiene ese ingrediente (Glazed Cake)
+            {
+                return false;
+            }
+            else //Aun no lo tiene (frutas), entonces lo agregamos
+            {
+                if (!hasFruit) //Si no tiene fruta, le ponemos fruta
+                {
+                    kitchenObjectSOList.Add(kitchenObjectSO);
+                    hasFruit = true;
+                    OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs
+                    {
+                        kitchenObjectSO = kitchenObjectSO
+                    });
+                    return true;
+                }
+                else //Si ya tiene algun tipo de fruta, no se puede agregar más
+                {
+                    return false;
+                }
+                
+            }
+        }
+        else //No tiene GlazedCake
+        {
+            if (kitchenObjectSO == glazedCakeKitchenObjectSO) //El primer ingrediente debe ser Glazed Cake
+            {
+                kitchenObjectSOList.Add(kitchenObjectSO);
+
+                OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs
+                {
+                    kitchenObjectSO = kitchenObjectSO
+                });
+                hasGlazedCake = true;
+                return true;
+            }
+            else
+            {
+                return false; //Si intenta agregar fruta antes que la base del pastel (GlazedCake), no podrá hacerlo
+            }
         }
         
-        if (kitchenObjectSOList.Contains(kitchenObjectSO)) //Ya tiene ese ingrediente
-        {            
-            return false;
-        }
-        else //Aun no lo tiene, entonces lo agregamos
-        {
-            kitchenObjectSOList.Add(kitchenObjectSO);
+    } 
 
-            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs
-            {
-                kitchenObjectSO = kitchenObjectSO
-            });
-            return true;
-        }
-    }
-    
 }
